8bd20fd6d0696dd91f64aa22a3ac373f3e5f1f45
rogervolz@gmail.com <guilherme.castelo@usp.br>
2023-09-15 19:45:30 -0300

    feat(datafow): add dataflow implementation

--- Exp3/interface_hcsr04_fd.vhd
+++ Exp3/interface_hcsr04_fd.vhd
@@ -27,20 +27,53 @@ architecture structural of interface_hcsr04_fd is
              pulso  : out std_logic;
              pronto : out std_logic
         );
-     end component;
+    end component;
 
-    component contador_bcd_3digitos is 
-    port ( 
-        clock   : in  std_logic;
-        zera    : in  std_logic;
-        conta   : in  std_logic;
-        digito0 : out std_logic_vector(3 downto 0);
-        digito1 : out std_logic_vector(3 downto 0);
-        digito2 : out std_logic_vector(3 downto 0);
-        fim     : out std_logic
-    );
+    component contador_cm is
+        generic (
+            constant R : integer;
+            constant N : integer
+        );
+        port (
+            clock   : in  std_logic;
+            reset   : in  std_logic;
+            pulso   : in  std_logic;
+            digito0 : out std_logic_vector(3 downto 0);
+            digito1 : out std_logic_vector(3 downto 0);
+            digito2 : out std_logic_vector(3 downto 0);
+            fim     : out std_logic;
+            pronto  : out std_logic
+        );
     end component;
 
 begin
+    PULSE_GENERATOR: gerador_pulso
+    generic map (
+        largura => 500 -- 10_000 / 20
+    )
+    port map (
+        clock  => clock,
+        reset  => reset,
+        gera   => gera,
+        para   => '0',
+        pulso  => trigger,
+        pronto => open
+    );
+
+    COUNTER_CM: contador_cm
+    generic map (
+        R => 2941, -- 58_820 / 20
+        N => 12
+    )
+    port map (
+        clock   => clock,
+        reset   => reset,
+        pulso   => pulso,
+        digito0 => distancia(3 downto 0),
+        digito1 => distancia(7 downto 4),
+        digito2 => distancia(11 downto 8),
+        fim     => open,
+        pronto  => fim_medida
+    );
 
 end architecture;
