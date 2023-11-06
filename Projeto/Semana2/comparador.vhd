4a4fcbbdb1006b9a9024be4af7c3bd982f643958
BnyioM <brunomaia.duarte@gmail.com>
2023-11-06 12:20:55 -0300

    feature/game

---
+++ Projeto/Semana2/comparador.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity comparador_n is
+    generic (
+        constant N: integer := 12 
+    );
+    port (
+        A      : in  std_logic_vector (N-1 downto 0);
+        B      : in  std_logic_vector (N-1 downto 0);
+        Bmaior : out std_logic;
+        igual  : out std_logic;
+        Bmenor : out std_logic
+    );
+end entity comparador_n;
+
+architecture comportamental of comparador_n is
+  signal intA, intB: integer;
+begin
+    intA <= to_integer(unsigned(A));
+    intB <= to_integer(unsigned(B));
+    process(A, B)
+        begin
+            Bmaior <= '0';
+            igual <= '0';
+            Bmenor <= '0';
+            if (intA=intB) then 
+              igual <= '0';
+            elsif (intB>intA) then
+              Bmaior <= '1';
+            else
+              Bmenor <= '1';
+            end if;
+        end process;
+end architecture comportamental;
--- Projeto/Semana2/trincheira_fd.vhd
+++ Projeto/Semana2/trincheira_fd.vhd
@@ -11,12 +11,29 @@ entity trincheira_fd is
         troca             : in  std_logic;
 		    detona			  	  : in  std_logic;
 		    cursores		  	  : in  std_logic_vector(3 downto 0);
+				medir							: in  std_logic;
+				limpa_sensor			: in  std_logic;
+				echo11						: in  std_logic;
+				echo12						: in  std_logic;
+				echo13						: in  std_logic;
+				echo21						: in  std_logic;
+				echo22						: in  std_logic;
+				echo32						: in  std_logic;
         atira1            : out std_logic;
         atira2            : out std_logic;
         horizontal1       : out std_logic;
         horizontal2       : out std_logic;
         vertical1         : out std_logic;
         vertical2         : out std_logic;
+				trigger11				  : out std_logic;
+				trigger12				  : out std_logic;
+				trigger13				  : out std_logic;
+				trigger21				  : out std_logic;
+				trigger22				  : out std_logic;
+				trigger32				  : out std_logic;
+				fim_medidas6      : out std_logic;
+				acertou_tudo      : out std_logic;
+				valido 						: out std_logic;
 		    vez					      : out std_logic;
         fim_atira         : out std_logic;
         faz_jogada        : out std_logic;
@@ -30,6 +47,32 @@ entity trincheira_fd is
 end entity;
 
 architecture structural of trincheira_fd is
+	component interface_hcsr04 is
+    port (
+				clock : in std_logic;
+				reset : in std_logic;
+				medir : in std_logic;
+				echo : in std_logic;
+				trigger : out std_logic;
+				medida : out std_logic_vector(11 downto 0); -- 3 digitos BCD
+				pronto : out std_logic;
+				db_estado : out std_logic_vector(3 downto 0) -- estado da UC
+    );
+	end component interface_hcsr04;
+
+	component comparador_n is
+    generic (
+        constant N: integer := 12 
+    );
+    port (
+        A      : in  std_logic_vector (N-1 downto 0);
+        B      : in  std_logic_vector (N-1 downto 0);
+        Bmaior : out std_logic;
+        igual  : out std_logic;
+        Bmenor : out std_logic
+    );
+	end component comparador_n;
+
 	component registrador_n is
 		generic (
 			constant N: integer := 8 
@@ -106,12 +149,21 @@ architecture structural of trincheira_fd is
 	   );
 	end component;
 
+	-- Sinais para controle dos servomotores e sinal do registrador da vez
+	signal s_vez_intermediario: std_logic_vector(0 downto 0);
 	signal s_vez, s_seletor_atira1, s_seletor_atira2, s_conta_direita, s_conta_esquerda, s_conta_cima, s_conta_baixo: std_logic;
 	signal s_conta_up_horizontal1, s_conta_down_horizontal1, s_conta_up_horizontal2, s_conta_down_horizontal2: std_logic;  
 	signal s_conta_up_vertical1, s_conta_down_vertical1, s_conta_up_vertical2, s_conta_down_vertical2: std_logic;
-	signal s_esquerda, s_direita, s_baixo, s_cima: std_logic;
-	signal s_vez_intermediario: std_logic_vector(0 downto 0);
 	signal s_atira1, s_atira2, s_horizontal1, s_horizontal2, s_vertical1, s_vertical2: std_logic_vector(2 downto 0);
+	-- Cursores
+	signal s_esquerda, s_direita, s_baixo, s_cima: std_logic;
+	-- Sinais da medicao de distancia
+	signal s_conta_medida: std_logic;
+	signal s_pronto11, s_pronto21, s_pronto31, s_pronto12, s_pronto22, s_pronto32: std_logic;
+	signal s_medida11, s_medida21, s_medida31, s_medida12, s_medida22, s_medida32: std_logic_vector(11 downto 0);
+	signal s_compara11, s_compara21, s_compara31, s_compara12, s_compara22, s_compara32: std_logic_vector(11 downto 0);
+	signal s_maior11, s_maior21, s_maior31, s_maior12, s_maior22, s_maior32: std_logic;
+	signal s_menor11, s_menor21, s_menor31, s_menor12, s_menor22, s_menor32: std_logic;
 begin
 	s_esquerda <= not(cursores(0));
 	s_direita <= not(cursores(1));
@@ -140,8 +192,8 @@ begin
 	
 	CONTA_ATIRA: contador_m
 		generic map (
-			M => 50_000_000, -- 100ms 
-			--M => 5_000, -- 100us para simulação
+			M => 50_000_000, -- 1s 
+			--M => 50_000, -- 1ms para simulação
 			N => 30
 		)
 		port map (
@@ -236,7 +288,7 @@ begin
 	CONTA_BAIXO: contador_m
 		generic map (
 			M => 5_000_000, -- 100ms 
-			--M => 5_000, -- 200us para simulação
+			--M => 5_000, -- 100us para simulação
 			N => 30
 		)
 		port map (
@@ -375,6 +427,227 @@ begin
 			db_posicao => open
 		);
 	
+	SENSOR_SOLDADO1JOG1: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo11,
+				trigger   => trigger11,
+				medida    => s_medida11,
+				pronto    => s_pronto11,
+				db_estado => open
+		);
+	REG_SOLDADO1JOG1: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto11,
+			D      => s_medida11,
+			Q      => s_compara11
+		);
+	COMP_SOLDADO1JOG1: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara21,
+        Bmaior => s_maior21,
+        igual  => open,
+        Bmenor => s_menor21
+    );
+	
+	SENSOR_SOLDADO2JOG1: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo21,
+				trigger   => trigger21,
+				medida    => s_medida21,
+				pronto    => s_pronto21,
+				db_estado => open
+		);
+	REG_SOLDADO2JOG1: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto21,
+			D      => s_medida21,
+			Q      => s_compara21
+		);
+	COMP_SOLDADO2JOG1: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara21,
+        Bmaior => s_maior21,
+        igual  => open,
+        Bmenor => s_menor21
+    );
+	
+	SENSOR_SOLDADO3JOG1: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo31,
+				trigger   => trigger31,
+				medida    => s_medida31,
+				pronto    => s_pronto31,
+				db_estado => open
+		);
+	REG_SOLDADO3JOG1: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto31,
+			D      => s_medida31,
+			Q      => s_compara31
+		);
+	COMP_SOLDADO3JOG1: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara31,
+        Bmaior => s_maior31,
+        igual  => open,
+        Bmenor => s_menor31
+    );
+
+	SENSOR_SOLDADO1JOG2: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo12,
+				trigger   => trigger12,
+				medida    => s_medida12,
+				pronto    => s_pronto12,
+				db_estado => open
+		);
+	REG_SOLDADO1JOG2: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto12,
+			D      => s_medida12,
+			Q      => s_compara12
+		);
+	COMP_SOLDADO1JOG2: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara12,
+        Bmaior => s_maior12,
+        igual  => open,
+        Bmenor => s_menor12
+    );
+
+	SENSOR_SOLDADO2JOG2: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo22,
+				trigger   => trigger22,
+				medida    => s_medida22,
+				pronto    => s_pronto22,
+				db_estado => open
+		);
+	REG_SOLDADO2JOG2: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto22,
+			D      => s_medida22,
+			Q      => s_compara22
+		);
+	COMP_SOLDADO2JOG2: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara22,
+        Bmaior => s_maior22,
+        igual  => open,
+        Bmenor => s_menor22
+    );
+	
+	SENSOR_SOLDADO3JOG2: interface_hcsr04
+		port map (
+				clock     => clock,
+				reset     => limpa_sensor,
+				medir     => medir,
+				echo      => echo32,
+				trigger   => trigger32,
+				medida    => s_medida32,
+				pronto    => s_pronto32,
+				db_estado => open
+		);
+	REG_SOLDADO3JOG2: registrador_n
+		generic map (
+			N => 12 
+		)
+		port map (
+			clock  => clock,
+			clear  => reset,
+			enable => s_pronto32,
+			D      => s_medida32,
+			Q      => s_compara32
+		);
+	COMP_SOLDADO3JOG2: comparador_n
+    generic map (
+        N => 12
+    )
+    port map (
+        A      => "000000100000",
+        B      => s_compara32,
+        Bmaior => s_maior32,
+        igual  => open,
+        Bmenor => s_menor32
+    );
+
+	s_conta_medida <= s_pronto11 or s_pronto21 or s_pronto31 or s_pronto12 or s_pronto22 or s_pronto32;
+	CONTA_MEDIDA: contador_m
+		generic map (
+			M => 6,
+			N => 3
+		)
+		port map (
+			clock => clock,
+			zera  => limpa_sensor,
+			conta => s_conta_medida,
+			Q     => open,
+			fim   => fim_medidas6,
+			meio  => open
+		);
+	-- Output
+	valido <= s_menor11 or s_menor21 or s_menor31 or s_menor12 or s_menor22 or s_menor32;
+	acertou_tudo <= (s_maior11 and s_maior21 and s_maior31) or (s_maior12 and s_maior22 and s_maior32);
 	vez <= s_vez;
 
 end architecture;
